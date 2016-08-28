08c44bd0dd08d5d540723633a4ce116e6a34b102
klpatil <kpatil@horizontalintegration.com>
2016-08-28 17:25:12 -0500

    Sitecore 8-2 : Solution

---
+++ sc-82-demo/sitecore/admin/MongoShell.aspx
@@ -0,0 +1,120 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+    CodeFile="MongoShell.aspx.cs"
+    Inherits="sc_82_demo.sitecore.admin.MongoShell" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Mongo Shell Tester - BETA</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+        crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+        .page-header {
+            padding-bottom: 9px;
+            margin: 62px 0 21px;
+            border-bottom: 1px solid #eee;
+        }
+    </style>
+</head>
+<body>
+    <form id="frmMongoShell" runat="server">
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <a class="navbar-brand" href="#">Mongo Shell&nbsp;<sup><span class="label label-warning">BETA</span></sup></a>
+                </div>
+
+                <!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <div class="container">
+
+
+            <div class="page-header">
+                <p class="lead">Mongo Shell - Helps you to validate your mongo connection, and do basic mongo checks!</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3">
+
+                    <label class="control-label">Mongo DB :</label>
+                    <asp:DropDownList runat="server" ID="ddlMongoDBs" CssClass="form-control">
+                    </asp:DropDownList>
+                </div>
+                <div class="col-md-3">
+                    <label class="control-label">Operation :</label>
+
+                    <asp:DropDownList runat="server" ID="ddlOperations" CssClass="form-control">
+                        <asp:ListItem Text="Test Connection" Value="TC" />
+                        <asp:ListItem Text="Find All Users" Value="FAC" />
+                        <asp:ListItem Text="Get Collection Names" Value="GCN" />
+                        <asp:ListItem Text="Get Collection" Value="GC" />
+                        <asp:ListItem Text="Get Stats" Value="GS" />
+                    </asp:DropDownList>
+                </div>
+                <div class="col-md-3">
+
+                    <label class="control-label">Input :</label>
+
+                    <asp:TextBox ID="txtInput" runat="server" CssClass="form-control" placeholder="Text input" />
+
+                </div>
+                <div class="col-md-3">
+                    <br />
+
+                    <asp:Button ID="btnTest" runat="server" OnClick="btnTest_Click" Text="Execute" CssClass="btn btn-primary" />
+                </div>
+            </div>
+            <div>
+                <p>&nbsp;</p>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Output : </h3>
+                </div>
+                <div class="panel-body">
+                    <asp:TextBox ID="txtResult" runat="server" TextMode="MultiLine" Rows="15" Columns="140" class="form-control" />
+
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </form>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax;/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ sc-82-demo/sitecore/admin/MongoShell.aspx.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.Text;
+using System.Web.UI.WebControls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceContentManagement.Administration;
+
+namespace sc_82_demo.sitecore.admin
+{
+    public partial class MongoShell : NonSecurePage
+    {
+        #region Private fields
+        enum MongoOperations
+        {
+            /// <summary>
+            /// Test Connection
+            /// </summary>
+            TC,
+            /// <summary>
+            /// Find All Users
+            /// </summary>
+            FAC,
+            /// <summary>
+            /// Get Collection Names
+            /// </summary>
+            GCN,
+            /// <summary>
+            /// Get Stats
+            /// </summary>
+            GS,
+            /// <summary>
+            /// Get Collection
+            /// </summary>
+            GC
+        }
+        private readonly StringBuilder Log = new StringBuilder();
+
+        #endregion
+
+        #region Protected methods or events
+        protected override void OnInit(EventArgs arguments)
+        {
+            Assert.ArgumentNotNull(arguments, "arguments");
+            base.CheckSecurity(true);
+            base.OnInit(arguments);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                foreach (ConnectionStringSettings setting in ConfigurationManager.ConnectionStrings)
+                {
+                    if (IsMongoConnectionString(setting))
+                    {
+                        ddlMongoDBs.Items.Add(new ListItem(setting.Name, setting.Name));
+
+                        //TestConnectionString(setting.Name, setting.ConnectionString);
+                    }
+                }
+            }
+        }
+
+        protected void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                //foreach (ConnectionStringSettings setting in ConfigurationManager.ConnectionStrings)
+                //{
+                //    if (IsMongoConnectionString(setting))
+                //    {
+                //        TestConnectionString(setting.Name, setting.ConnectionString);
+                //    }
+                //}
+
+                if (ddlMongoDBs.SelectedIndex == -1 || ddlOperations.SelectedIndex == -1)
+                {
+                    Log.AppendLine();
+                    // Show Messsage
+                    Log.AppendLine("No MongoDB Selected or No Operation selected. Please select");
+                }
+                else
+                {
+                    MongoOperations mongoOperations = (MongoOperations)
+                        Enum.Parse(typeof(MongoOperations), ddlOperations.SelectedItem.Value, true);
+
+                    var connectionString = ConfigurationManager.ConnectionStrings[ddlMongoDBs.SelectedItem.Value].ConnectionString;
+                    switch (mongoOperations)
+                    {
+                        case MongoOperations.TC:
+                            TestConnectionString(ddlMongoDBs.SelectedItem.Text, connectionString);
+                            break;
+                        case MongoOperations.FAC:
+                        case MongoOperations.GCN:
+                        case MongoOperations.GS:
+                            DoMongoOperation(ddlMongoDBs.SelectedItem.Text, connectionString, mongoOperations);
+                            break;
+                        case MongoOperations.GC:
+                            if (string.IsNullOrWhiteSpace(txtInput.Text))
+                                Log.AppendLine("For Get collection operation. Collection Name is required. Please provide it via Input box");
+                            else
+                                DoMongoOperation(ddlMongoDBs.SelectedItem.Text, connectionString, mongoOperations);
+                            break;
+                        default:
+                            Log.AppendLine("Invalid operation");
+                            break;
+                    }
+
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Log.AppendLine($"{exception.Message}{exception.InnerException}{exception.StackTrace}");
+
+            }
+            DumpLog(Log);
+        }
+
+        #endregion
+
+        #region Private methods
+        private void DoMongoOperation(string name, string connectionString, MongoOperations mongoOperation)
+        {
+            Log.AppendLine($"====== Test Date: {DateTime.Now.ToString(CultureInfo.InvariantCulture)} ======");
+            Log.AppendLine($"Connection Name: {name}.");
+            Log.AppendLine($"Connection String: {connectionString}.");
+
+            try
+            {
+                MongoClient client = new MongoClient(connectionString);
+
+                // http://stackoverflow.com/questions/7201847/how-to-get-the-mongo-database-specified-in-connection-string-in-c-sharp
+                // TODO : Authentication
+                var database = client.GetServer().GetDatabase(new MongoUrl(connectionString).DatabaseName);
+
+                Log.AppendLine($"====== Operation Name: {ddlOperations.SelectedItem} ======");
+                switch (mongoOperation)
+                {
+                    case MongoOperations.FAC:
+                        var users = database.FindAllUsers();
+
+                        foreach (MongoUser mongouser in users)
+                        {
+                            Log.AppendLine($"Mongo Username: {mongouser.Username}.");
+                        }
+                        break;
+                    case MongoOperations.GCN:
+                        var collectionNames = database.GetCollectionNames();
+                        foreach (string collectionName in collectionNames)
+                        {
+                            Log.AppendLine($"Mongo Collection Name: {collectionName}.");
+                        }
+                        break;
+                    case MongoOperations.GS:
+                        var stats = database.GetStats();
+
+                        Log.AppendLine($"CollectionCount: {stats.CollectionCount}.");
+                        Log.AppendLine($"DataSize: {Sitecore.StringUtil.GetSizeString(stats.DataSize)}.");
+                        //Log.AppendLine($"FileSize: {Sitecore.StringUtil.GetSizeString(stats.FileSize)}.");
+                        Log.AppendLine($"IndexSize: {Sitecore.StringUtil.GetSizeString(stats.IndexSize)}.");
+                        Log.AppendLine($"IndexCount: {stats.IndexCount}.");
+                        Log.AppendLine($"OK?: {stats.Ok}.");
+                        Log.AppendLine($"StorageSize: {Sitecore.StringUtil.GetSizeString(stats.StorageSize)}.");
+                        break;
+                    case MongoOperations.GC:
+                        var collection = database.GetCollection(txtInput.Text);
+                        foreach (BsonDocument document in collection.FindAll())
+                        {
+                            Log.AppendLine($"Mongo Document : {document.ToString()}.");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                Log.AppendLine($"{exception.Message}{exception.InnerException}{exception.StackTrace}{mongoOperation.ToString()}");
+            }
+        }
+        
+        /// <summary>
+        /// If string contains tracking it is Mongo
+        /// </summary>
+        /// <param name="setting"></param>
+        /// <returns></returns>
+        private static bool IsMongoConnectionString(ConnectionStringSettings setting)
+        {
+            return setting.ConnectionString.StartsWith("mongodb://");
+        }
+
+        private void TestConnectionString(string name, string connectionString)
+        {
+            Log.AppendLine($"====== Test Date: {DateTime.Now.ToString(CultureInfo.InvariantCulture)} ======");
+            Log.AppendLine($"Connection Name: {name}.");
+            Log.AppendLine($"Connection String: {connectionString}.");
+
+            try
+            {
+                MongoClient client = new MongoClient(connectionString);
+
+                if (client != null && client.GetServer() != null)
+                {
+                    if (client.GetServer().State == MongoServerState.Connected)
+                    {
+                        var versionString = client.GetServer().BuildInfo.VersionString;
+                        Log.AppendLine($"MongoDB version: {versionString}.");
+                        Log.AppendLine("Wohoo -- Connection is successful!");
+                    }
+                    else
+                    {
+                        Log.AppendLine("Ooops -- Unable to make connection.");
+                    }
+                }
+               
+            }
+            catch (Exception exception)
+            {
+
+                Log.AppendLine($"{exception.Message}{exception.InnerException}{exception.StackTrace}");
+            }
+        }
+
+        private void DumpLog(StringBuilder log)
+        {
+
+            txtResult.Text = log.ToString();
+            //Console.WriteLine(log);
+
+            //var fileName = $"Support.Log.{DateTime.Now.ToString("yyyyMMdd.hhmmss")}.txt";
+
+            //using (StreamWriter writer = new StreamWriter(fileName))
+            //{
+            //    writer.Write(log);
+            //}
+        }
+        #endregion
+    }
+}
